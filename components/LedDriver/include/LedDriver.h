655804de0a48d20badf92d85839b8dbbb9a28c5f
BrianGel <brian.geldmacher@web.de>
2023-07-04 09:46:47 +0200

    Feat: Add LedDriver component

---
+++ components/LedDriver/include/LedDriver.h
@@ -0,0 +1,13 @@
+#ifndef D_LedDriver_H
+#define D_LedDriver_H
+
+void LedDriver_Create(void);
+void LedDriver_Destroy(void);
+
+void toggle_led(void);
+
+// void configure_led(void)
+// void blink_led(void)
+
+
+#endif  /* D_LedDriver_H */
